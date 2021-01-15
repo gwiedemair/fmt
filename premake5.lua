6ef0d00eee03c8153acc81b8db241bf3c4c3fa62
gwiedemair <g.wiedemair@gmail.com>
2021-01-15 11:37:35 +0100

    Cleaning premake

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,6 @@ project "fmt"
 	location ""
 	language "C++"
 	cppdialect "C++latest"
-	staticruntime "on"
-
 
 	defines {
 	}
@@ -26,6 +24,7 @@ project "fmt"
 
 	filter "platforms:Static"
 		kind "StaticLib"
+
 	filter "platforms:Shared"
 		kind "SharedLib"
 		defines "FMT_EXPORT"
@@ -33,26 +32,3 @@ project "fmt"
 			-- ("{COPY} \"%{wks.location}/bin/" .. outputdir .. "/FoWSI/*.dll\" %{cfg.targetdir}")
 		}
 
-
-	filter "system:windows"
-		systemversion "latest"
-		defines {
-		}
-
-	filter "configurations:Debug"
-		defines {
-		}
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines {
-		}
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines {
-		}
-		runtime "Release"
-		optimize "on"
